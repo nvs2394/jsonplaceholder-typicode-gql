2f6fd03808d6ea8b82201ead2d317242d0b26510
Son Nguyen <nvs2394@gmail.com>
2017-12-14 22:45:07 +0700

    Fix package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const config = require('config');
 // Application entrypoint. Actually starts the server.
 const server = require('./server');
 
 // Start the server, detail out what's going on.
-server.start();
+server.start((err) => {
+  console.log(`server started at port: ${config.get('App.port')}`)
+});
