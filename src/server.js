2a33c8fbee18a1da01fe05ac3985a12cb7c0e48e
Son Nguyen <nvs2394@gmail.com>
2017-12-15 09:08:34 +0700

    Fix deploy heroku

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const Config = require('config');
 const plugins = require('./plugin');
 async function StartServer () {
   const server = new hapi.Server({
-    port: Config.get('App.port')
+    port: Config.get('App.port'),
+    host: '0.0.0.0'
   });
 
   await server.register(plugins);
